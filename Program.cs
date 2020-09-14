a94e3087c9477d02efe433f9f0630cf200d63e89
KellyCristinaAires <k.woys@hotmail.com>
2020-09-13 23:34:44 -0300

    atividade contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace contagemregressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("preparar...");
+            Console.WriteLine("Pressione enter para continuar...");
+            Console.ReadKey();
+
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("Pressione enter para continuar...");
+            Console.ReadKey(); 
+
+            Console.WriteLine("Fogo"!);
+            Console.WriteLine("pressione enter para sair!");
+            Console.ReadKey();
+        }
+    }
+}
